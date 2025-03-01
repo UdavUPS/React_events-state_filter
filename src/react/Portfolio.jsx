d762e4cd2d08738f5e36f0313828948a7d8974a3
Udav-94 <ups-fti@yandex.ru>
2025-03-01 17:32:57 +0300

    Внедрение uniqid вместо Math.random для генерации key в элементах

--- src/react/Portfolio.jsx
+++ src/react/Portfolio.jsx
@@ -1,3 +1,5 @@
+import uniqid from 'uniqid';
+
 export function Portfolio({items}) {
 
 
@@ -5,7 +7,7 @@ export function Portfolio({items}) {
         <>
         {items.map( (item) => {
             return (
-            <div className="pBox" category={item.category} key={Math.random()}>
+            <div className="pBox" category={item.category} key={uniqid()}>
                 <img className="pBox__itme" src={item.img} alt="" />
             </div>
             );
